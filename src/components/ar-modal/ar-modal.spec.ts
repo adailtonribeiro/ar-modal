8055a005e8cf971caff954698ed12c95a1ef9bb6
adailtonribeiro <adailton.ribeiro1@gmail.com>
2018-05-29 07:33:32 -0300

    fix tests

--- src/components/ar-modal/ar-modal.spec.ts
+++ src/components/ar-modal/ar-modal.spec.ts
@@ -1,43 +1,27 @@
-import { TestWindow } from '@stencil/core/testing';
-import { MyComponent } from './ar-modal';
+import {TestWindow} from '@stencil/core/testing';
+import {ArModal} from './ar-modal';
 
 describe('ar-modal', () => {
   it('should build', () => {
-    expect(new MyComponent()).toBeTruthy();
+    expect(new ArModal()).toBeTruthy();
   });
 
   describe('rendering', () => {
-    let element: HTMLMyComponentElement;
+    let element: HTMLArModalElement;
     let testWindow: TestWindow;
     beforeEach(async () => {
       testWindow = new TestWindow();
       element = await testWindow.load({
-        components: [MyComponent],
+        components: [ArModal],
         html: '<ar-modal></ar-modal>'
       });
     });
 
-    it('should work without parameters', () => {
-      expect(element.textContent.trim()).toEqual('Hello, World! I\'m');
+    it('should work with a title', async () => {
+      element.modalTitle = 'Modal Title';
+      await testWindow.flush();
+      expect(element.textContent.trim()).toContain('Modal Title');
     });
 
-    it('should work with a first name', async () => {
-      element.first = 'Peter';
-      await testWindow.flush();
-      expect(element.textContent.trim()).toEqual('Hello, World! I\'m Peter');
-    });
-
-    it('should work with a last name', async () => {
-      element.last = 'Parker';
-      await testWindow.flush();
-      expect(element.textContent.trim()).toEqual('Hello, World! I\'m  Parker');
-    });
-
-    it('should work with both a first and a last name', async () => {
-      element.first = 'Peter'
-      element.last = 'Parker';
-      await testWindow.flush();
-      expect(element.textContent.trim()).toEqual('Hello, World! I\'m Peter Parker');
-    });
   });
 });
